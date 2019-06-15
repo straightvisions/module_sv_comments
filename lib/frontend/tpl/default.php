bd77f29ef27c7c14abeaabeaf5abfb17d7b75207
Matthias Bathke <info@straightvisions.com>
2019-06-15 12:06:04 +0200

    RENAME - VARS

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -1,12 +1,12 @@
 <div class="<?php echo $this->get_prefix(); ?>">
 	<h3 class="<?php echo $this->get_prefix( 'title' ); ?>">
 		<?php
-		$comments = get_comments( array(
+		$post_comments = get_comments( array(
 				'post_id' 						=> get_the_ID() ,
 				'status'						=> 'approve'
 		));
 
-		echo count( $comments ) . ' ' . ( count( $comments ) == 1
+		echo count( $post_comments ) . ' ' . ( count( $post_comments ) == 1
 				? __( 'Comment', 'straightvisions_100' )
 				: __( 'Comments', 'straightvisions_100' )
 			);
@@ -16,32 +16,32 @@
 	<?php
 	$i = 0;
 
-	foreach ( $comments as $comment ) {
-		$author = get_user_by( 'email', $comment->comment_author_email );
+	foreach ( $post_comments as $post_comment ) {
+		$author = get_user_by( 'email', $post_comment->comment_author_email );
 	?>
 
-    <div id="comment-<?php echo $comment->comment_ID; ?>" class="<?php echo $this->get_prefix( 'comment' ); ?>">
+    <div id="comment-<?php echo $post_comment->comment_ID; ?>" class="<?php echo $this->get_prefix( 'comment' ); ?>">
         <div class="<?php echo $this->get_prefix( 'author_avatar_wrapper' ); ?>">
 	        <div class="<?php echo $this->get_prefix( 'author_avatar' ); ?>">
 		        <?php
-		        if ( get_avatar( $comment->comment_author_email, '80px' ) ) {
-			        echo get_avatar( $comment->comment_author_email, '80px' );
+		        if ( get_avatar( $post_comment->comment_author_email, '80px' ) ) {
+			        echo get_avatar( $post_comment->comment_author_email, '80px' );
 		        }
 		        ?>
             </div>
         </div>
         <div class="<?php echo $this->get_prefix( 'comment_content_wrapper' ); ?>">
             <span class="<?php echo $this->get_prefix( 'author' ); ?>">
-	            <?php echo ( $comment->comment_author_url
-					? '<a href="' . $comment->comment_author_url . '" target="_blank">' . $comment->comment_author . '</a>'
-					: $comment->comment_author ); ?>
+	            <?php echo ( $post_comment->comment_author_url
+					? '<a href="' . $post_comment->comment_author_url . '" target="_blank">' . $post_comment->comment_author . '</a>'
+					: $post_comment->comment_author ); ?>
             </span>
             <span class="<?php echo $this->get_prefix( 'comment_date' ); ?>">
                 <?php
-                echo get_comment_date( '', $comment->comment_ID );
+                echo get_comment_date( '', $post_comment->comment_ID );
                 ?>
             </span>
-            <span class="<?php echo $this->get_prefix( 'comment_content' ); ?>"><?php echo $comment->comment_content; ?></span>
+            <span class="<?php echo $this->get_prefix( 'comment_content' ); ?>"><?php echo $post_comment->comment_content; ?></span>
         </div>
     </div>
 
