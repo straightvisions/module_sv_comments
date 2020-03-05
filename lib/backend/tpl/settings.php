99c9052facd14178b0b703cbaaa6a81202adfc7a
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2020-03-05 14:36:40 +0100

    Removed run_type() from setting calls.

--- lib/backend/tpl/settings.php
+++ lib/backend/tpl/settings.php
@@ -6,29 +6,29 @@
 		<h3 class="divider"><?php _e( 'Texts', 'sv100' ); ?></h3>
 		<div class="sv_setting_flex">
 			<?php
-				echo $module->get_settings_component( 'font_family' )->run_type()->form();
-				echo $module->get_settings_component( 'font_size' )->run_type()->form();
-				echo $module->get_settings_component( 'line_height' )->run_type()->form();
-				echo $module->get_settings_component( 'text_color' )->run_type()->form();
+				echo $module->get_settings_component( 'font_family' )->form();
+				echo $module->get_settings_component( 'font_size' )->form();
+				echo $module->get_settings_component( 'line_height' )->form();
+				echo $module->get_settings_component( 'text_color' )->form();
 			?>
 		</div>
 
 		<h3 class="divider"><?php _e( 'Titles', 'sv100' ); ?></h3>
 		<div class="sv_setting_flex">
 			<?php
-				echo $module->get_settings_component( 'font_family_title' )->run_type()->form();
-				echo $module->get_settings_component( 'font_size_title' )->run_type()->form();
-				echo $module->get_settings_component( 'line_height_title' )->run_type()->form();
-				echo $module->get_settings_component( 'text_color_title' )->run_type()->form();
+				echo $module->get_settings_component( 'font_family_title' )->form();
+				echo $module->get_settings_component( 'font_size_title' )->form();
+				echo $module->get_settings_component( 'line_height_title' )->form();
+				echo $module->get_settings_component( 'text_color_title' )->form();
 			?>
 		</div>
 
 		<h3 class="divider"><?php _e( 'Colors', 'sv100' ); ?></h3>
 		<div class="sv_setting_flex">
 			<?php
-				echo $module->get_settings_component( 'bg_color' )->run_type()->form();
-				echo $module->get_settings_component( 'highlight_color' )->run_type()->form();
-				echo $module->get_setting( 'author_color' )->run_type()->form();
+				echo $module->get_settings_component( 'bg_color' )->form();
+				echo $module->get_settings_component( 'highlight_color' )->form();
+				echo $module->get_setting( 'author_color' )->form();
 			?>
 		</div>
 		<?php
